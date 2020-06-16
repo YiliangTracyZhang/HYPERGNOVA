9ecfe38e57165a9fb50c5d5c0b9f75154918414d
qlu-lab <yiliang.zhang@yale.edu>
2020-06-16 12:28:08 -0400

    update

--- calculate.py
+++ calculate.py
@@ -23,105 +23,64 @@ def nearest_Corr(input_mat):
     return A
 
 
-def calLocalCov(i, partition, geno_array, coords, bps, gwas_snps, ld_scores, n1, n2, pheno_corr, pheno_corr_var):
-    m = len(gwas_snps)
-    CHR = partition.iloc[i, 0]
-    START = partition.iloc[i, 1]
-    END = partition.iloc[i, 2]
+def calLocalCov(i, tmp_partition, geno_array1, geno_array2, coords, bps, tmp_gwas_snps, tmp_flip, n1, n2):
+    m = len(tmp_gwas_snps)
+    CHR = tmp_partition.iloc[i, 0]
+    START = tmp_partition.iloc[i, 1]
+    END = tmp_partition.iloc[i, 2]
 
-    idx = np.logical_and(np.logical_and(gwas_snps['CHR']==CHR, bps <= END), bps >= START)
+    idx = np.logical_and(np.logical_and(tmp_gwas_snps['CHR']==CHR, bps <= END), bps >= START)
     m0 = np.sum(idx)
     if m0 < 120:
-        df = pd.DataFrame(OrderedDict({"chr":[], "start":[], "end":[], "rho":[], "corr":[], "h2_1":[], "h2_2":[], "var":[], "p":[], "m":[]}))
+        df = pd.DataFrame(OrderedDict({"chr":[], "start":[], "end":[], "rho":[], "m":[]}))
         return df
     
     tmp_coords = coords[idx]
 
-    block_gwas_snps = gwas_snps[idx]
-    block_ld_scores = ld_scores[idx]
-    max_dist = 0.03
+    block_gwas_snps = tmp_gwas_snps[idx]
+    block_flip = tmp_flip[idx]
+    max_dist = 1
     block_left = ld.getBlockLefts(tmp_coords, max_dist)
 
-    lN, blockLD = geno_array.ldCorrVarBlocks(block_left, idx)
-    lN = block_ld_scores["L2"]
-    meanLD = np.mean(lN)
-    local_LD = nearest_Corr(blockLD)
+    blockLD1 = geno_array1.ldCorrVarBlocks(block_left, idx)
+    local_LD1 = nearest_Corr(blockLD1)
+    blockLD2 = geno_array2.ldCorrVarBlocks(block_left, idx)
+    flip_multiply = block_flip * 2 - 1
+    blockLD2 = blockLD2 * flip_multiply
+    blockLD2 = blockLD2.T * flip_multiply
+    local_LD2 = nearest_Corr(blockLD2)
 
-    d, v = linalg.eigh(local_LD)
-    order = d.argsort()[::-1]
-    d = d[order]
-    v = v[:,order]
-    if np.sum(d>0) < 120:
-        df = pd.DataFrame(OrderedDict({"chr":[], "start":[], "end":[], "rho":[], "corr":[], "h2_1":[], "h2_2":[], "var":[], "p":[], "m":[]}))
+    d1, v1 = linalg.eigh(local_LD1)
+    d2, v2 = linalg.eigh(local_LD2)
+    order1 = d1.argsort()[::-1]
+    order2 = d2.argsort()[::-1]
+
+    d1 = d1[order1]
+    v1 = v1[:,order1]
+    d2 = d2[order2]
+    v2 = v2[:,order2]
+    if np.sum(np.logical_and(d1>0, d2>0)) < 120:
+        df = pd.DataFrame(OrderedDict({"chr":[], "start":[], "end":[], "rho":[], "m":[]}))
         return df
     
-    sub_d = d[d>0]
-    sub_v = v[:,d>0]
 
-    tz1 = np.dot(sub_v.T, block_gwas_snps['Z_x'])
-    tz2 = np.dot(sub_v.T, block_gwas_snps['Z_y'])
-    y = tz1 * tz2 - pheno_corr * sub_d
+    sub_d1 = d1[np.logical_and(d1>0, d2>0)]
+    sub_v1 = v1[:,np.logical_and(d1>0, d2>0)]
 
-    Localh1 = (np.mean(block_gwas_snps['Z_x'] ** 2) - 1) / meanLD * m0 / n1
-    Localh2 = (np.mean(block_gwas_snps['Z_y'] ** 2) - 1) / meanLD * m0 / n2
+    sub_d2 = d2[np.logical_and(d1>0, d2>0)]
+    sub_v2 = v2[:,np.logical_and(d1>0, d2>0)]
 
-    Z_x = gwas_snps['Z_x']
-    Z_y = gwas_snps['Z_y']
+    tz1 = np.dot(sub_v1.T, block_gwas_snps['Z_x'])
+    tz2 = np.dot(sub_v2.T, block_gwas_snps['Z_y'])
+    y = tz1 * tz2
+    sub_v = sub_v1 * sub_v2
+    u = sub_v.sum(axis=0)
+    numerator = y.T.dot(u)
+    w = sub_d1 * sub_d2
+    denominator = w.T.dot(np.square(u))
+    rho = m0 / sqrt(n1 * n2) * numerator / denominator
 
-    h1 = (np.mean(Z_x ** 2) - 1) / np.mean(ld_scores['L2']) * m / n1
-    h2 = (np.mean(Z_y ** 2) - 1) / np.mean(ld_scores['L2']) * m / n2
-
-    wh1 = h1 * m0 / m
-    wh2 = h2 * m0 / m
-    #wh12 = np.max([Localh1, 0])
-    #wh22 = np.max([Localh2, 0])
-    #wh1 = (wh11 + wh12) / 2
-    #wh2 = (wh21 + wh22) / 2
-    Localrho = (np.sum(block_gwas_snps['Z_x'] * block_gwas_snps['Z_y']) - pheno_corr * m0) / meanLD / sqrt(n1 * n2)
-
-    threshold = 1
-    cur_d = sub_d[sub_d>threshold]
-    cur_y = y[sub_d>threshold]
-    cur_dsq = cur_d ** 2
-    denominator = (wh1 * cur_d / m0 + 1 / n1) * (wh2 * cur_d / m0 + 1 / n2)
-    cur_v1 = np.sum(cur_dsq / denominator)
-    cur_v2 = np.sum(cur_y / sqrt(n1 * n2) / denominator)
-    cur_v3 = np.sum(cur_y ** 2 / (n1 * n2) / (denominator * cur_dsq))
-
-    emp_var = [(cur_v3 - (cur_v2 ** 2) / cur_v1) / (cur_v1 * (len(cur_d) - 1))]
-    theo_var = [1 / cur_v1]
-
-    for K in range(len(cur_d), len(sub_d)):
-        eig = sub_d[K]
-        tmp_y = y[K]
-        cur_v1 += eig ** 2 / ((wh1 * eig / m0 + 1 / n1) * (wh2 * eig / m0 + 1 / n2))
-        cur_v2 += tmp_y / sqrt(n1 * n2) / ((wh1 * eig / m0 + 1 / n1) * (wh2 * eig / m0 + 1 / n2))
-        cur_v3 += tmp_y ** 2 / (n1 * n2) / ((wh1 * eig ** 2 / m0 + eig / n1) * (wh2 * eig ** 2 / m0 + eig / n2))
-        emp_var.append((cur_v3 - (cur_v2 ** 2) / cur_v1) / (cur_v1 * K))
-        theo_var.append(1 / cur_v1)
-    
-    max_emp_theo = np.maximum(emp_var, theo_var)
-    min_idx = np.argmin(max_emp_theo)
-
-    y = y[:(len(cur_d)+min_idx-1)]
-    sub_d = sub_d[:(len(cur_d)+min_idx-1)]
-    sub_dsq = sub_d ** 2
-
-    var_rho = m0 ** 2 * min(max_emp_theo)
-    q = (wh1 * sub_d / m0 + 1 / n1) * (wh2 * sub_d / m0 + 1 / n2)
-    v4 = np.sum(sub_d/q)/np.sum(sub_dsq/q)
-    var_phencorr = pheno_corr_var / (n1 * n2) * m0 ** 2 * v4 ** 2
-    var_rho += var_phencorr
-
-    se_rho = sqrt(var_rho)
-    p_value = norm.sf(abs(Localrho / se_rho)) * 2
-
-    if Localh1 < 0 or Localh2 < 0:
-        corr = np.nan
-    else:
-        corr = Localrho / sqrt(Localh1 * Localh2)
-
-    df = pd.DataFrame(OrderedDict({"chr":[CHR], "start":[START], "end":[END], "rho":[Localrho], "corr":[corr], "h2_1":[Localh1], "h2_2":[Localh2], "var":[var_rho], "p":[p_value], "m":[m0]}))
+    df = pd.DataFrame(OrderedDict({"chr":[CHR], "start":[START], "end":[END], "rho":[rho], "m":[m0]}))
 
     return df
 
@@ -326,7 +285,7 @@ def calculate(bfile1, bfile2, partition, thread, gwas_snps, reversed_alleles_ref
             else:
                 df = _supergnova_global(bfile1, bfile2, partition, thread, gwas_snps, reversed_alleles_ref, n1, n2)
         total_m = np.sum(df['m'])
-        global_Cov = total_m / sqrt(n1 * n2) * np.sum(df['numerator'] / np.sum(df['denominator']))
+        global_Cov = total_m / sqrt(n1 * n2) * np.sum(df['numerator']) / np.sum(df['denominator'])
         results = pd.DataFrame(OrderedDict({"rho":[global_Cov], "m":[total_m]}))
         convert_dict = {"m":int}
         results = results.astype(convert_dict)
